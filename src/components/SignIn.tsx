38fbfbe3d067239d824b5686d061c45927844a9c
bearone236 <kuma_2306@icloud.com>
2024-06-07 16:27:52 +0900

    サインインコンポーネントの作成

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function SignIn() {
+  return (
+    <div>
+      <Button
+        variant="secondary"
+        className="text-white font-semibold bg-orange-400 hover:bg-orange-500"
+      >
+        Sign In
+      </Button>
+    </div>
+  );
+}
