39a385f8b2862343cefd6850789e18b6f8165d51
bearone236 <kuma_2306@icloud.com>
2024-06-07 16:27:17 +0900

    ホーム画面の作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,207 @@
-import Image from "next/image";
+"use client";
+
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import * as z from "zod";
+
+const schema = z.object({
+  selectedTheme: z.string().min(1, "テーマが選択されていません"),
+  theme: z.string().optional(),
+  selectedThinkTime: z.string().min(1, "シンキングタイムが選択されていません"),
+  thinkTime: z
+    .string()
+    .refine((value) => !isNaN(Number(value)), {
+      message: "シンキングタイムは数値でなければなりません",
+    })
+    .optional(),
+  selectedSpeakTime: z
+    .string()
+    .min(1, "スピーキングタイムが選択されていません"),
+  speakTime: z
+    .string()
+    .refine((value) => !isNaN(Number(value)), {
+      message: "スピーキングタイムは数値でなければなりません",
+    })
+    .optional(),
+});
+
+type SchemaType = z.infer<typeof schema>;
 
 export default function Home() {
+  const [selectedTheme, setSelectedTheme] = useState<string>("");
+  const [theme, setTheme] = useState<string>("");
+  const [selectedThinkTime, setSelectedThinkTime] = useState<string>("");
+  const [thinkTime, setThinkTime] = useState<string>("");
+  const [selectedSpeakTime, setSelectedSpeakTime] = useState<string>("");
+  const [speakTime, setSpeakTime] = useState<string>("");
+  const [errors, setErrors] = useState<
+    Partial<Record<keyof SchemaType, string>>
+  >({});
+
+  const validate = (): boolean => {
+    const result = schema.safeParse({
+      selectedTheme,
+      theme,
+      selectedThinkTime,
+      thinkTime,
+      selectedSpeakTime,
+      speakTime,
+    });
+
+    if (!result.success) {
+      const newErrors: Partial<Record<keyof SchemaType, string>> = {};
+      result.error.errors.forEach((error) => {
+        newErrors[error.path[0] as keyof SchemaType] = error.message;
+      });
+      setErrors(newErrors);
+      return false;
+    }
+
+    setErrors({});
+    return true;
+  };
+
+  const handleStartClick = () => {
+    if (validate()) {
+      // スタートボタンがクリックされた時の処理
+      console.log("スタート");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <Header />
+
+      <main className="flex justify-center items-center min-h-screen bg-gray-50">
+        <div className="w-full max-w-4xl mx-auto p-6 space-y-8 bg-white shadow-md rounded-lg pt-20 pb-10">
+          <div className="space-y-8">
+            <div className="flex items-start space-x-4 pl-14">
+              <h3 className="w-1/4 text-right font-medium flex items-center">
+                テーマ
+              </h3>
+              <div className="flex-grow flex flex-col space-y-2">
+                <div className="flex items-center space-x-4">
+                  <Select onValueChange={(value) => setSelectedTheme(value)}>
+                    <SelectTrigger className="w-[250px]">
+                      <SelectValue placeholder="テーマを設定してください" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="random">ランダムテーマ</SelectItem>
+                      <SelectItem value="specify">テーマ指定</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  {selectedTheme === "specify" && (
+                    <Input
+                      type="text"
+                      className="w-[250px]"
+                      placeholder="テーマを入力してください"
+                      onChange={(e) => setTheme(e.target.value)}
+                    />
+                  )}
+                </div>
+                <div className="h-5">
+                  {errors.selectedTheme && (
+                    <p className="text-red-500">{errors.selectedTheme}</p>
+                  )}
+                  {errors.theme && selectedTheme === "specify" && (
+                    <p className="text-red-500">{errors.theme}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-start space-x-4 pl-14">
+              <h3 className="w-1/4 text-right font-medium flex items-center">
+                シンキングタイム(秒)
+              </h3>
+              <div className="flex-grow flex flex-col space-y-2">
+                <div className="flex items-center space-x-4">
+                  <Select
+                    onValueChange={(value) => setSelectedThinkTime(value)}
+                  >
+                    <SelectTrigger className="w-[250px]">
+                      <SelectValue placeholder="タイムを設定してください" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="10s">10秒</SelectItem>
+                      <SelectItem value="30s">30秒</SelectItem>
+                      <SelectItem value="60s">60秒</SelectItem>
+                      <SelectItem value="custom-think">カスタム</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  {selectedThinkTime === "custom-think" && (
+                    <Input
+                      type="text"
+                      className="w-[250px]"
+                      placeholder="タイムを入力してください"
+                      onChange={(e) => setThinkTime(e.target.value)}
+                    />
+                  )}
+                </div>
+                <div className="h-5">
+                  {errors.selectedThinkTime && (
+                    <p className="text-red-500">{errors.selectedThinkTime}</p>
+                  )}
+                  {errors.thinkTime && selectedThinkTime === "custom-think" && (
+                    <p className="text-red-500">{errors.thinkTime}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-start space-x-4 pl-14">
+              <h3 className="w-1/4 text-right font-medium flex items-center">
+                スピーキングタイム(秒)
+              </h3>
+              <div className="flex-grow flex flex-col space-y-2">
+                <div className="flex items-center space-x-4">
+                  <Select
+                    onValueChange={(value) => setSelectedSpeakTime(value)}
+                  >
+                    <SelectTrigger className="w-[250px]">
+                      <SelectValue placeholder="時間を設定してください" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="60s">60秒</SelectItem>
+                      <SelectItem value="120s">120秒</SelectItem>
+                      <SelectItem value="180s">180秒</SelectItem>
+                      <SelectItem value="custom-speak">カスタム</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  {selectedSpeakTime === "custom-speak" && (
+                    <Input
+                      type="text"
+                      className="w-[250px]"
+                      placeholder="時間を入力してください"
+                      onChange={(e) => setSpeakTime(e.target.value)}
+                    />
+                  )}
+                </div>
+                <div className="h-5">
+                  {errors.selectedSpeakTime && (
+                    <p className="text-red-500">{errors.selectedSpeakTime}</p>
+                  )}
+                  {errors.speakTime && selectedSpeakTime === "custom-speak" && (
+                    <p className="text-red-500">{errors.speakTime}</p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <Button onClick={handleStartClick}>スタート</Button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
