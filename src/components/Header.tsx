8fd23f424ed27ebf6b5b10a700bf4ecbb2d6a985
bearone236 <kuma_2306@icloud.com>
2024-06-09 14:58:19 +0900

    Headerコンポーネントのネスト移動

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 import SignIn from "./SignIn";
 export default function Header() {
   return (
     <header className="flex justify-between py-2 px-5 bg-orange-100 items-center">
-      <h1 className="font-semibold">English Skills App</h1>
+      <Link href="/">
+        <h1 className="font-semibold">English Skills App</h1>
+      </Link>
       <SignIn />
     </header>
   );
